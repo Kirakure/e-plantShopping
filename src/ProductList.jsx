238e1b0b7e838d9e265f0cf9eb3d5f62df94d33f
Kirakure <urbantechtree+github@gmail.com>
2025-03-29 10:37:24 -0400

    Third Commit

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -292,6 +292,8 @@ function ProductList({ onHomeClick }) {
                              <div className='product-card' key={plantIndex}>
                                  <img className='product-image' src={plant.image} alt={plant.name} />
                                  <div className='product-title'>{plant.name}</div>
+                                 <div className='product-price'>{plant.cost}</div>
+                                 <div className='product-description'>{plant.description}</div>
                                  <button className='product-button' onClick={()=> handleAddToCart(plant) }>Add to Cart</button>
                              </div>
                          ))}
